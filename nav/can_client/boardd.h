8a0ae74a3a62e0e602bdc93efcdb9fb34320ab34
Sameh Mohamed <sameh.mohamed@motional.com>
2023-11-27 08:49:31 -0500

    cleaning up to prepare for send CAN feature

--- nav/can_client/boardd.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "commaai/can_client.h"
-
-#include <string>
-#include <vector>
-
-bool safety_setter_thread(std::vector<Panda *> pandas);
-void boardd_main_thread(std::vector<std::string> serials);
