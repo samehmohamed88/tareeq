1fd04a6750701f6def3cc16b6ac6482e06da9b34
Sameh Mohamed <sameh.mohamed@motional.com>
2023-11-30 08:43:59 -0500

    send back some EyeSight messages to prevent fault

--- nav/can_client/standalone-bins/SendAndReceieveSteeringCommandCANMessage.cc
+++ nav/can_client/standalone-bins/SendAndReceieveSteeringCommandCANMessage.cc
@@ -18,13 +18,113 @@
 
 using namespace nav;
 
+bool EXIT = false;
+
+// dont send steering command right away
+// but send everything else
+bool DO_SEND = false;
+
+int COUNTER = 0;
+
+void UpdateLKASStateMessage(can::CANMessage &message) {
+    for (auto signal : message.signals) {
+        if (signal.name == "LKAS_Alert_Msg") {
+            signal.value = 0;
+        } else if (signal.name == "LKAS_Alert") {
+            signal.value = 0;
+        } else if (signal.name == "LKAS_Alert_Msg") {
+            signal.value = 0;
+        } else if (signal.name == "LKAS_ACTIVE") {
+            signal.value = 1;
+        } else if (signal.name == "COUNTER") {
+            signal.value++;
+        }
+    }
+}
+
+void UpdateESDistanceMessage(can::CANMessage &message) {
+    for (auto signal : message.signals) {
+        if (signal.name == "Cruise_Soft_Disable") {
+            signal.value = 0;
+        } else if (signal.name == "Cruise_Fault") {
+            signal.value = 0;
+        }
+    }
+}
+
+std::vector<can::CANMessage> ProcessMessages(std::vector<can::CANMessage> &messages) {
+    std::vector<can::CANMessage> messageToSend{};
+    for (auto &message : messages) {
+        if (message.name == "ES_LKAS_State") {
+            UpdateLKASStateMessage(message);
+            can::CANMessage msg = message;
+            messageToSend.push_back(msg);
+        } else if (message.name == "ES_Distance") {
+            UpdateESDistanceMessage(message);
+            can::CANMessage msg = message;
+            messageToSend.push_back(msg);
+        }
+    }
+    return messageToSend;
+}
+
+void CreateAndSendSteeringMessage(can::CommaAICANInterfaceWithBoostBuffer<can::UsbDevice<can::LibUsbDevice>>& canDevice) {
+    std::vector<can::CANMessage::CANSignal> signals{};
+    signals.push_back(can::CANMessage::CANSignal{
+                              0,
+                              "SET_1",
+                              1});
+    signals.push_back(can::CANMessage::CANSignal{
+                              0,
+                              "LKAS_Output",
+                              9.0});
+    signals.push_back(can::CANMessage::CANSignal{
+            0,
+            "LKAS_Request",
+            1});
+    can::CANMessage message = can::CANMessage{290,"ES_LKAS",std::move(signals)};
+    std::vector<uint8_t> sentData = canDevice.sendMessages(std::vector<can::CANMessage>{message});
+}
+
+std::thread CreateReceiveThread(can::CommaAICANInterfaceWithBoostBuffer<can::UsbDevice<can::LibUsbDevice>>& canDevice) {
+    std::vector<uint8_t> chunck{};
+    auto thread = std::thread([&]{
+        while (!EXIT) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(10));
+            canDevice.receiveMessages(chunck, true);
+        }
+    });
+    return thread;
+}
+
+std::thread CreateSendThread(can::CommaAICANInterfaceWithBoostBuffer<can::UsbDevice<can::LibUsbDevice>>& canDevice) {
+    std::vector<uint8_t> chunck{};
+    auto thread = std::thread([&]{
+        while (!EXIT) {
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+            auto messages = canDevice.getCANMessagesAndClearContainer();
+            auto messagesToSend = ProcessMessages(messages);
+            canDevice.sendMessages(messages);
+            ++COUNTER;
+            if (COUNTER % 5 == 0 && DO_SEND) {
+                // let's send a steering command
+                CreateAndSendSteeringMessage(canDevice);
+            }
+        }
+    });
+    return thread;
+}
+
+
+
 int main() {
     using namespace nav;
     static constexpr uint16_t vendorID_ = 0xbbaa;
     static constexpr uint16_t productID_ = 0xddcc;
 
-    std::unordered_map<std::string , int32_t > messageMap{};
 
+//    std::unordered_map<std::string , int32_t > messageMap{};
+//
     auto device = std::make_unique<can::UsbDevice<can::LibUsbDevice>>(
             std::make_unique<can::LibUsbDevice>(),
                     vendorID_,
@@ -35,101 +135,18 @@ int main() {
     auto hw = canDevice.getHardwareType();
     assert(hw == 7);
     std::cout << "Hardware Type == " << std::to_string(hw) << std::endl;
+    bool valid = canDevice.setSafetyModel(can::SafetyModel::AllOutput, 1);
+    std::cout << "Safety Model set " << valid << std::endl;
 
+    auto sendThread = CreateSendThread(canDevice);
+    auto receiveThread = CreateReceiveThread(canDevice);
 
-//    auto thread1 = std::thread([&]{
-//        while (true) {
-//            auto messages = canDevice.getCANMessagesAndClearContainer();
-////            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-////            std::cout << " size of messages " << messages.size() << std::endl;
-//            for (auto const& message : messages) {
-////                std::cout << "Message name is "
-////                          << message.name
-////                          << " and address is "
-////                          << message.address
-////                          << " and signal size "
-////                          << message.signals.size() << std::endl;
-//
-//                if (messageMap.find(message.name) != messageMap.end()) {
-//                    messageMap[message.name]++;
-//                } else {
-//                    messageMap[message.name] = 1;
-//                }
-//                for (auto const& signal : message.signals) {
-//                    if (signal.name == "CHECKSUM" || signal.name == "COUNTER") {
-//                        continue;
-//                    }
-//                    parsedData << message.name << ",";
-//                    parsedData << signal.name << ",";
-//                    parsedData << signal.value;
-//                    parsedData << "\n";
-//                }
-//            }
-//        }
-//    });
-//
-//
-//    thread1.join();
-//    std::vector<can::CANMessage::CANSignal> signals{};
-//    signals.push_back(can::CANMessage::CANSignal{
-//                              0,
-//                              "SET_1",
-//                              1});
-//    signals.push_back(can::CANMessage::CANSignal{
-//                              0,
-//                              "LKAS_Output",
-//                              9.0});
-//    signals.push_back(can::CANMessage::CANSignal{
-//            0,
-//            "LKAS_Request",
-//            1});
-//
-//    can::CANMessage message = can::CANMessage{290,"ES_LKAS",std::move(signals)};
-//
-//    bool valid = canDevice.setSafetyModel(can::SafetyModel::AllOutput, 1);
-//    std::cout << valid << std::endl;
-//    for (int i = 0 ; i < 10; i++) {
-//        std::cout << "sending message " << std::endl;
-//        std::vector<uint8_t> sentData = canDevice.sendMessages(std::vector<can::CANMessage>{message});
-//        std::this_thread::sleep_for(std::chrono::milliseconds(10));
-//    }
+    sendThread.join();
+    receiveThread.join();
 
-
-    std::string filename = "/apollo/nav/can_client/data/write_and_read_LKAS_message.bin"; // Replace with your filename
-//
-//    std::ofstream outFile(filename, std::ios::binary);
-//    // Check if the file is open
-//    if (!outFile.is_open()) {
-//        std::cerr << "Error opening file for writing." << std::endl;
-//        return 1;
-//    }
-//    // Write the vector data to the file
-//    outFile.write(reinterpret_cast<const char*>(sentData.data()), sentData.size());
-//
-//    // Close the file
-//    outFile.close();
-//
-//    // Check for errors during write
-//    if (!outFile) {
-//        std::cerr << "Error writing to file." << std::endl;
-//        return 1;
-//    }
-//
-//    // Now open the file, and read it, and parse it to verify it's the same message
-    int chunk = 1024;
-    std::vector<uint8_t> buffer(chunk);
-    std::ifstream file(filename, std::ios::binary);
-    file.read(reinterpret_cast<char*>(buffer.data()), chunk);
-    size_t bytesRead = file.gcount();
-    buffer.resize(bytesRead); // Resize buffer to actual bytes read
-    canDevice.receiveMessages(buffer, false);
-//
-//    // now get the message  off the  queue
-    auto messages = canDevice.getCANMessagesAndClearContainer();
-//    assert(messages[0].name  == message.name);
-//    assert(messages[0].address  == message.address);
-//    assert(messages[0].signals[0].value  == message.signals[0].value);
-
+    if (COUNTER % 10000 == 0) {
+        EXIT = true;
+    }
 
     return 0;
 }
