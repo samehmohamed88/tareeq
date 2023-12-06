2d0efbb588291138e40156b5ba478490b42fb9d1
Sameh Mohamed <sameh4@gmx.com>
2023-12-06 11:14:25 -0500

    adding socket can support

---
+++ nav/can_client/CANDBCSignalSchema.cc
@@ -0,0 +1,152 @@
+#include "nav/can_client/CANDBCSignalSchema.h"
+
+namespace nav {
+namespace can {
+/// This is taken from CommaAI openpilot
+/// reference: https://github.com/commaai/opendbc/blob/2b96bcc45669cdd14f9c652b07ef32d6403630f6/can/common.cc#L27C1-L27C1
+uint8_t CANDBCSignalSchema::calcSubaruChecksum(uint32_t address, const std::vector<uint8_t> &d) const {
+    unsigned int s = 0;
+    while (address) {
+        s += address & 0xFF; address >>= 8;
+    }
+    // skip checksum in first byte
+    for (size_t i = 1; i < d.size(); i++) {
+        s += d[i];
+    }
+    return s & 0xFF;
+};
+
+int64_t CANDBCSignalSchema::parseValue(const std::vector<uint8_t> &messageData) {
+    int64_t ret = 0;
+    int i = mostSignificantBit_ / 8;
+    int bits = size_;
+    while (i >= 0 && i < static_cast<int>(messageData.size()) && bits > 0) {
+        int lsb = (int)(leastSignificantBit_ / 8) == i ? leastSignificantBit_ : i*8;
+        int msb = (int)(mostSignificantBit_ / 8) == i ? mostSignificantBit_ : (i+1)*8 - 1;
+        int size = msb - lsb + 1;
+
+        uint64_t d = (messageData[i] >> (lsb - (i*8))) & ((1ULL << size) - 1);
+        ret |= d << (bits - size);
+
+        bits -= size;
+        i = isLittleEndian() ? i-1 : i+1;
+    }
+    return ret;
+}
+
+void CANDBCSignalSchema::packValue(std::vector<uint8_t> &writeBuffer, int64_t tmp_val) const {
+    int i = leastSignificantBit_ / 8;
+    int bits = size_;
+    if (size_ < 64) {
+        tmp_val &= ((1ULL << size_) - 1);
+    }
+
+    while (i >= 0 && i < static_cast<int>(writeBuffer.size()) && bits > 0) {
+        int shift = (int)(leastSignificantBit_ / 8) == i ? leastSignificantBit_ % 8 : 0;
+        int size = std::min(bits, 8 - shift);
+
+        writeBuffer[i] &= ~(((1ULL << size) - 1) << shift);
+        writeBuffer[i] |= (tmp_val & ((1ULL << size) - 1)) << shift;
+
+        bits -= size;
+        tmp_val >>= size;
+        i = isLittleEndian() ? i+1 : i-1;
+    }
+}
+
+const std::string &CANDBCSignalSchema::getName() const {
+    return name_;
+}
+
+const std::string &CANDBCSignalSchema::getMessageName() const {
+    return messageName_;
+}
+
+int CANDBCSignalSchema::getStartBit() const {
+    return startBit_;
+}
+
+int CANDBCSignalSchema::getMostSignificantBit() const {
+    return mostSignificantBit_;
+}
+
+int CANDBCSignalSchema::getLeastSignificantBit() const {
+    return leastSignificantBit_;
+}
+
+int CANDBCSignalSchema::getSize() const {
+    return size_;
+}
+bool CANDBCSignalSchema::isSigned() const {
+    return isSigned_;
+}
+
+double CANDBCSignalSchema::getFactor() const {
+    return factor_;
+}
+double CANDBCSignalSchema::getOffset() const {
+    return offset_;
+}
+
+bool CANDBCSignalSchema::isLittleEndian() const {
+    return isLittleEndian_;
+}
+
+const SignalType &CANDBCSignalSchema::getSignalType() const {
+    return type_;
+}
+
+void CANDBCSignalSchema::setMessageName(const std::string &messageName) {
+    messageName_ = messageName;
+}
+
+void CANDBCSignalSchema::setStartBit(int startBit) {
+    startBit_ = startBit;
+}
+
+void CANDBCSignalSchema::setMostSignificantBit(int mostSignificantBit) {
+    mostSignificantBit_ = mostSignificantBit;
+}
+
+void CANDBCSignalSchema::setLeastSignificantBit(int leastSignificantBit) {
+    leastSignificantBit_ = leastSignificantBit;
+}
+
+void CANDBCSignalSchema::setSize(int size) {
+    size_ = size;
+}
+
+void CANDBCSignalSchema::setIsSigned(bool isSigned) {
+    isSigned_ = isSigned;
+}
+
+void CANDBCSignalSchema::setFactor(double factor) {
+    factor_ = factor;
+}
+
+void CANDBCSignalSchema::setOffset(double offset) {
+    offset_ = offset;
+}
+
+void CANDBCSignalSchema::setIsLittleEndian(bool isLittleEndian) {
+    isLittleEndian_ = isLittleEndian;
+}
+
+SignalType CANDBCSignalSchema::getType() const {
+    return type_;
+}
+
+void CANDBCSignalSchema::setType(SignalType type) {
+    type_ = type;
+}
+
+const CANDBCSignalSchema::ValueDescription &CANDBCSignalSchema::getValueDescription() const {
+    return valueDescription_;
+}
+
+void CANDBCSignalSchema::setValueDescription(const CANDBCSignalSchema::ValueDescription &valueDescription) {
+    valueDescription_ = valueDescription;
+}
+
+}
+}
--- nav/can_client/CANDBCSignalSchema.h
+++ nav/can_client/CANDBCSignalSchema.h
@@ -17,65 +17,63 @@ public:
         uint32_t address;
         std::string def_val;
     };
-    std::string name;
-    std::string messageName;
-    int startBit, mostSignificantBit, leastSignificantBit, size;
-    bool is_signed;
-    double factor, offset;
-    bool isLittleEndian;
-    SignalType type;
+    const std::string &getName() const;
+    const std::string &getMessageName() const;
+    int getStartBit() const;
+    int getMostSignificantBit() const;
+    int getLeastSignificantBit() const;
+    int getSize() const;
+    bool isSigned() const;
+    double getFactor() const;
+    double getOffset() const;
+    bool isLittleEndian() const;
+    const SignalType &getSignalType() const;
+    uint8_t calcSubaruChecksum(uint32_t address, const std::vector<uint8_t> &d) const;
+    int64_t parseValue(const std::vector<uint8_t> &messageData);
+
+    void setName(std::string name);
+
+    void setMessageName(const std::string &messageName);
+
+    void setStartBit(int startBit);
+
+    void setMostSignificantBit(int mostSignificantBit);
+
+    void setLeastSignificantBit(int leastSignificantBit);
+
+    void setSize(int size);
+
+    void setIsSigned(bool isSigned);
+
+    void setFactor(double factor);
+
+    void setOffset(double offset);
+
+    void setIsLittleEndian(bool isLittleEndian);
+
+    SignalType getType() const;
+
+    void setType(SignalType type);
+
+    const ValueDescription &getValueDescription() const;
+
+    void setValueDescription(const ValueDescription &valueDescription);
+
+    void packValue(std::vector<uint8_t> &writeBuffer, int64_t tmp_val) const;
+private:
+    std::string name_;
+    std::string messageName_;
+    int startBit_;
+    int mostSignificantBit_;
+    int leastSignificantBit_ ;
+    int size_;
+    bool isSigned_;
+    double factor_;
+    double offset_;
+    bool isLittleEndian_;
+    SignalType type_;
     ValueDescription valueDescription_;
-    /// This is taken from CommaAI openpilot
-    /// reference: https://github.com/commaai/opendbc/blob/2b96bcc45669cdd14f9c652b07ef32d6403630f6/can/common.cc#L27C1-L27C1
-    uint8_t calcSubaruChecksum(uint32_t address, const std::vector<uint8_t> &d) const {
-        unsigned int s = 0;
-        while (address) {
-            s += address & 0xFF; address >>= 8;
-        }
-        // skip checksum in first byte
-        for (size_t i = 1; i < d.size(); i++) {
-            s += d[i];
-        }
-        return s & 0xFF;
-    };
 
-    int64_t parseValue(const std::vector<uint8_t> &messageData) {
-        int64_t ret = 0;
-        int i = mostSignificantBit / 8;
-        int bits = size;
-        while (i >= 0 && i < static_cast<int>(messageData.size()) && bits > 0) {
-            int lsb = (int)(leastSignificantBit / 8) == i ? leastSignificantBit : i*8;
-            int msb = (int)(mostSignificantBit / 8) == i ? mostSignificantBit : (i+1)*8 - 1;
-            int size = msb - lsb + 1;
-
-            uint64_t d = (messageData[i] >> (lsb - (i*8))) & ((1ULL << size) - 1);
-            ret |= d << (bits - size);
-
-            bits -= size;
-            i = isLittleEndian ? i-1 : i+1;
-        }
-        return ret;
-    }
-
-    void packValue(std::vector<uint8_t> &writeBuffer, int64_t tmp_val) const {
-        int i = leastSignificantBit / 8;
-        int bits = size;
-        if (size < 64) {
-            tmp_val &= ((1ULL << size) - 1);
-        }
-
-        while (i >= 0 && i < static_cast<int>(writeBuffer.size()) && bits > 0) {
-            int shift = (int)(leastSignificantBit / 8) == i ? leastSignificantBit % 8 : 0;
-            int size = std::min(bits, 8 - shift);
-
-            writeBuffer[i] &= ~(((1ULL << size) - 1) << shift);
-            writeBuffer[i] |= (tmp_val & ((1ULL << size) - 1)) << shift;
-
-            bits -= size;
-            tmp_val >>= size;
-            i = isLittleEndian ? i+1 : i-1;
-        }
-    }
 };
 
 
