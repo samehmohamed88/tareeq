ed85c59f030a7890f34ab99e7f81ed6dee8cd4ec
Sameh Mohamed <sameh.mohamed@motional.com>
2024-01-21 20:56:16 -0500

    solved behavior planner

--- courses/udacity/02_behavior_planning/03_behavior_planner/cost.h
+++ courses/udacity/02_behavior_planning/03_behavior_planner/cost.h
@@ -7,24 +7,24 @@ using std::map;
 using std::string;
 using std::vector;
 
-float calculate_cost(const Vehicle &vehicle, 
-                     const map<int, vector<Vehicle>> &predictions, 
-                     const vector<Vehicle> &trajectory);
+float calculate_cost(const Vehicle& vehicle,
+                     const map<int, vector<Vehicle>>& predictions,
+                     const vector<Vehicle>& trajectory);
 
-float goal_distance_cost(const Vehicle &vehicle,  
-                         const vector<Vehicle> &trajectory,  
-                         const map<int, vector<Vehicle>> &predictions, 
-                         map<string, float> &data);
+float goal_distance_cost(const Vehicle& vehicle,
+                         const vector<Vehicle>& trajectory,
+                         const map<int, vector<Vehicle>>& predictions,
+                         map<string, float>& data);
 
-float inefficiency_cost(const Vehicle &vehicle, 
-                        const vector<Vehicle> &trajectory, 
-                        const map<int, vector<Vehicle>> &predictions, 
-                        map<string, float> &data);
+float inefficiency_cost(const Vehicle& vehicle,
+                        const vector<Vehicle>& trajectory,
+                        const map<int, vector<Vehicle>>& predictions,
+                        map<string, float>& data);
 
-float lane_speed(const map<int, vector<Vehicle>> &predictions, int lane);
+float lane_speed(const map<int, vector<Vehicle>>& predictions, int lane);
 
-map<string, float> get_helper_data(const Vehicle &vehicle, 
-                                   const vector<Vehicle> &trajectory, 
-                                   const map<int, vector<Vehicle>> &predictions);
+map<string, float> get_helper_data(const Vehicle& vehicle,
+                                   const vector<Vehicle>& trajectory,
+                                   const map<int, vector<Vehicle>>& predictions);
 
-#endif  // COST_H
+#endif // COST_H
