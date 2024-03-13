23e7dfa815f8a388f4b3df88dab464445ccb4a27
Sameh Mohamed <sameh.mohamed@motional.com>
2024-03-13 13:04:41 -0400

    now have a plot of time series, data changing over time

--- av/localization/examples/chatgpt_extended_kalman_filter.rs
+++ av/localization/examples/chatgpt_extended_kalman_filter.rs
@@ -4,15 +4,11 @@ use rand::thread_rng;
 use rand::prelude::*;
 use rand_distr::StandardNormal;
 
-
+use plotters::prelude::*;
+use gtk::{prelude::*, DrawingArea};
+use plotters_cairo::CairoBackend;
 
 const DT: f64 = 0.1;
-const W: usize = 800;
-const H: usize = 600;
-
-const SAMPLE_RATE: f64 = 10_000.0;
-const FRAME_RATE: f64 = 30.0;
-
 
 fn deg2rad(deg: f64) -> f64 {
     deg * PI / 180.0
@@ -176,3 +172,43 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     Ok(())
 }
+
+fn build_ui(app: &gtk::Application) {
+    let window = gtk::ApplicationWindow::new(app);
+    // window.set_title("Sine and Cosine Graph");
+    window.set_default_size(800, 600);
+
+    let drawing_area = DrawingArea::new();
+    window.set_child(Some(&drawing_area));
+
+    drawing_area.set_draw_func(|_, cr, width, height| {
+        let root = CairoBackend::new(cr, (800, 600)).unwrap().into_drawing_area();
+        root.fill(&WHITE).unwrap();
+
+        let mut chart = ChartBuilder::on(&root)
+            .margin(5)
+            .x_label_area_size(30)
+            .y_label_area_size(30)
+            .build_cartesian_2d(-3.14..3.14, -1.5..1.5)
+            .unwrap();
+
+        chart.configure_mesh().draw().unwrap();
+
+        chart
+            .draw_series(LineSeries::new(
+                (-314..314).map(|x| x as f64 / 100.0).map(|x| (x, x.sin())),
+                &RED,
+            ))
+            .unwrap();
+
+        chart
+            .draw_series(LineSeries::new(
+                (-314..314).map(|x| x as f64 / 100.0).map(|x| (x, x.cos())),
+                &BLUE,
+            ))
+            .unwrap();
+    });
+
+    window.show();
+}
+
--- samples/plotters-gtk-simple/src/main.rs
+++ samples/plotters-gtk-simple/src/main.rs
@@ -1,49 +1,73 @@
-use gtk::{prelude::*, DrawingArea};
+use gtk::{glib, prelude::*, Application, ApplicationWindow, DrawingArea};
 use plotters::prelude::*;
 use plotters_cairo::CairoBackend;
+use std::rc::Rc;
+use std::cell::RefCell;
+
+const DT: f64 = 0.1;
 
 fn main() {
-    // Initialize GTK.
-    let application = gtk::Application::new(Some("com.example.Graph"), Default::default());
-    application.connect_activate(build_ui);
+    let application = Application::new(Some("com.example.Graph"), Default::default());
+    application.connect_activate(|app| {
+        let (drawing_area, data) = build_ui(app);
+        update_data_and_redraw(drawing_area, data);
+    });
     application.run();
 }
 
-fn build_ui(app: &gtk::Application) {
-    let window = gtk::ApplicationWindow::new(app);
-    // window.set_title("Sine and Cosine Graph");
+fn build_ui(app: &gtk::Application) -> (DrawingArea, Rc<RefCell<Vec<(f64, f64)>>>){
+    let window = ApplicationWindow::new(app);
     window.set_default_size(800, 600);
 
     let drawing_area = DrawingArea::new();
     window.set_child(Some(&drawing_area));
 
-    drawing_area.set_draw_func(|_, cr, width, height| {
+    // Initialize data
+    let data = Rc::new(RefCell::new(vec![]));
+
+    let data_clone = data.clone();
+    drawing_area.set_draw_func(move |_, cr, width, height| {
         let root = CairoBackend::new(cr, (800, 600)).unwrap().into_drawing_area();
         root.fill(&WHITE).unwrap();
+        let data = data_clone.borrow();
 
-        let mut chart = ChartBuilder::on(&root)
-            .margin(5)
-            .x_label_area_size(30)
-            .y_label_area_size(30)
-            .build_cartesian_2d(-3.14..3.14, -1.5..1.5)
-            .unwrap();
+        if !data.is_empty() {
+            let mut chart = ChartBuilder::on(&root)
+                .margin(5)
+                .x_label_area_size(30)
+                .y_label_area_size(30)
+                .build_cartesian_2d(0f64..10f64, -1.5f64..1.5f64)
+                .unwrap();
 
-        chart.configure_mesh().draw().unwrap();
+            chart.configure_mesh().draw().unwrap();
 
-        chart
-            .draw_series(LineSeries::new(
-                (-314..314).map(|x| x as f64 / 100.0).map(|x| (x, x.sin())),
+            chart.draw_series(LineSeries::new(
+                data.iter().cloned(),
                 &RED,
-            ))
-            .unwrap();
-
-        chart
-            .draw_series(LineSeries::new(
-                (-314..314).map(|x| x as f64 / 100.0).map(|x| (x, x.cos())),
-                &BLUE,
-            ))
-            .unwrap();
+            )).unwrap();
+        }
     });
 
     window.show();
+
+    (drawing_area, data)
+}
+
+fn update_data_and_redraw(drawing_area: DrawingArea, data: Rc<RefCell<Vec<(f64, f64)>>>){
+    let mut x = 0f64;
+    glib::timeout_add_seconds_local(1, move || {
+        // Simulate new data
+        x += 1.0;
+        let y = (x / 10.0).sin();
+        data.borrow_mut().push((x, y));
+
+        // Keep the data vector within a reasonable size
+        if x > 10.0 {
+            data.borrow_mut().remove(0);
+        }
+
+        drawing_area.queue_draw();
+
+        glib::Continue(true)
+    });
 }
