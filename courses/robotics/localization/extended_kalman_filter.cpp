06ce2ef231ad3eb4df30965918995ff4de66c6e2
Sameh Mohamed <sameh.mohamed@motional.com>
2024-03-15 11:45:45 -0400

    fixing orig ekf impl from cpp robotics lib

--- courses/robotics/localization/extended_kalman_filter.cpp
+++ courses/robotics/localization/extended_kalman_filter.cpp
@@ -14,6 +14,7 @@
 #include <iostream>
 #include <numbers>
 #include <random>
+#include <tuple>
 
 static constexpr double SIM_TIME = 50.0;
 static constexpr double DT = 0.1;
@@ -64,7 +65,7 @@ auto make_R() -> Eigen::Matrix2d
 
 auto make_input_noise() -> Eigen::Matrix2d
 {
-    double linear_noise = 1.0;                 // Linear noise
+    double linear_noise = 1.0;                              // Linear noise
     double angular_noise = 30.0 * std::numbers::pi / 180.0; // Convert degrees to radians for angular noise
 
     return make_2x2_diagonal(linear_noise, angular_noise);
@@ -78,10 +79,9 @@ auto make_gpu_noise() -> Eigen::Matrix2d
 auto make_jacob_H() -> Eigen::Matrix<double, 2, 4>
 {
     Eigen::Matrix<double, 2, 4> jH_;
-    jH_ << 1.0, 0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0;
+    jH_ << 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0;
     return jH_;
-};
+}
 
 /// @brief Jacobian of Motion Model
 ///
@@ -106,8 +106,7 @@ auto jacob_F(Eigen::Vector4d x, Eigen::Vector2d u) -> Eigen::Matrix4d
     jF_(1, 2) = DT * v * std::cos(yaw);
     jF_(1, 3) = DT * std::sin(yaw);
     return jF_;
-};
-
+}
 
 Eigen::Vector2d calc_input()
 {
@@ -121,203 +120,101 @@ Eigen::Vector2d calc_input()
 }
 
 // x_{t+1} = F @ x_{t} + B @ u_{t}
-auto motion_model(const Eigen::Vector4d& x, const Eigen::Vector2d& u) -> Eigen::Vector4d
+auto motion_model(Eigen::Matrix<double, 4, 1> &x, const Eigen::Matrix<double, 2, 1> &u)
 {
     Eigen::Matrix4d F_;
-    F_ << 1.0, 0.0, 0.0, 0.0,
-          0.0, 1.0, 0.0, 0.0,
-          0.0, 0.0, 1.0, 0.0,
-          0.0, 0.0, 0.0, 1.0;
+    F_ << 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0;
 
     Eigen::Matrix<double, 4, 2> B_;
-    B_ << DT * std::cos(x(2, 0)), 0.0,
-          DT * std::sin(x(2, 0)), 0.0,
-          0.0, DT,
-          1.0, 0.0;
+    B_ << DT * std::cos(x(2, 0)), 0.0, DT * std::sin(x(2, 0)), 0.0, 0.0, DT, 1.0, 0.0;
 
-    return F_ * x + B_ * u;
-};
+    x = F_ * x + B_ * u;
+}
 
-// observation mode H
-auto observation_model(const Eigen::Vector4d& x) -> Eigen::Vector2d
+auto observation_model(const Eigen::Matrix<double, 4, 1> &x) -> Eigen::Matrix<double, 2, 1>
 {
     Eigen::Matrix<double, 2, 4> H_;
-    H_ << 1.0, 0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0, 0.0;
+    H_ << 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0;
     return H_ * x;
-};
+}
 
-void ekf_estimation(Eigen::Vector4d& xEst,
-                    Eigen::Matrix4d& PEst,
-                    const Eigen::Vector2d& z,
-                    const Eigen::Vector2d& u)
+Eigen::Matrix<double, 2, 1> randomNormalVector() {
+    std::random_device rd;  // Obtain a random number from hardware
+    std::mt19937 gen(rd()); // Seed the generator
+    std::normal_distribution<> distr(0.0, 1.0); // Mean 0.0, standard deviation 1.0
+
+    Eigen::Matrix<double, 2, 1> result;
+    for (int i = 0; i < result.size(); ++i) {
+        result(i) = distr(gen); // Fill each element with a normally distributed random number
+    }
+
+    return result;
+}
+
+auto observation(Eigen::Matrix<double, 4, 1> &xTrue,
+                 Eigen::Matrix<double, 4, 1> &xDeadReckoning,
+                 const Eigen::Matrix<double, 2, 1> &u) -> std::tuple< Eigen::Matrix<double, 2, 1>,  Eigen::Matrix<double, 2, 1>>
+{
+    auto static const GPU_NOISE = make_gpu_noise();
+    auto static const INPUT_NOISE = make_input_noise();
+
+    // update xTrue by applying the motion model
+    motion_model(xTrue, u);
+
+    auto z = observation_model(xTrue) + GPU_NOISE * randomNormalVector();
+
+    // add noise to input
+    auto ud = u + INPUT_NOISE * randomNormalVector();
+
+    motion_model(xDeadReckoning, ud);
+
+    return {z, ud};
+}
+
+void ekf_estimation(Eigen::Matrix<double, 4, 1> &xEst, Eigen::Matrix<double, 4, 4> &PEst, const Eigen::Matrix<double, 2, 1> &z, const Eigen::Matrix<double, 2, 1> &u)
 {
     auto static const Q = make_Q();
     auto static const R = make_R();
     auto static const jH = make_jacob_H();
 
-    auto xPred = motion_model(xEst, u);
-    auto jF = jacob_F(xPred, u);
+    auto xPred = xEst;
+    motion_model(xPred, u);
 
-    Eigen::Vector2d PPred = jF * PEst * jF.transpose() + Q;
+    auto jF = jacob_F(xEst, u);
 
-    auto zPred = observation_model(xPred);
+    auto PPred = jF * PEst * jF.transpose() + Q;
+
+    auto zPred = observation_model(xEst);
+
     auto y = z - zPred;
-    Eigen::Matrix<double, 4, 2> S = jH * PPred * jH.transpose() + R;
-    Eigen::Matrix<double, 4, 2> K = PPred * jH.transpose() * S.inverse();
+    auto S = jH * PPred * jH.transpose() + R;
+    auto K = PPred * jH.transpose() * S.inverse();
+
     xEst = xPred + K * y;
-    PEst = (Eigen::Matrix4f::Identity() - K * jH) * PPred;
-};
-
-
-cv::Point2i cv_offset(Eigen::Vector2f e_p, int image_width = 2000, int image_height = 2000)
-{
-    cv::Point2i output;
-    output.x = int(e_p(0) * 100) + image_width / 2;
-    output.y = image_height - int(e_p(1) * 100) - image_height / 3;
-    return output;
-};
-
-void ellipse_drawing(cv::Mat bg_img,
-                     Eigen::Matrix2f pest,
-                     Eigen::Vector2f center,
-                     cv::Scalar ellipse_color = cv::Scalar(0, 0, 255))
-{
-    Eigen::EigenSolver<Eigen::Matrix2f> ces(pest);
-    Eigen::Matrix2f e_value = ces.pseudoEigenvalueMatrix();
-    Eigen::Matrix2f e_vector = ces.pseudoEigenvectors();
-
-    double angle = std::atan2(e_vector(0, 1), e_vector(0, 0));
-    cv::ellipse(bg_img,
-                cv_offset(center, bg_img.cols, bg_img.rows),
-                cv::Size(e_value(0, 0) * 1000, e_value(1, 1) * 1000),
-                angle / PI * 180,
-                0,
-                360,
-                ellipse_color,
-                2,
-                4);
-};
+    PEst = (Eigen::Matrix4d::Identity() - K * jH) * PPred;
+}
 
 int main()
 {
     float time = 0.0;
 
+    Eigen::Matrix<double, 4, 1> xEst = Eigen::Matrix<double, 4, 1>::Zero();
+    Eigen::Matrix<double, 4, 1> xTrue = Eigen::Matrix<double, 4, 1>::Zero();
+    Eigen::Matrix4d PEst = Eigen::Matrix4d::Identity();
+    Eigen::Matrix<double, 4, 1> xDeadReckoning = Eigen::Matrix<double, 4, 1>::Zero();
 
-    auto const INPUT_NOISE = make_input_noise();
-    auto GPU_NOISE = make_gpu_noise();
+    while (SIM_TIME >= time) {
+        time += DT;
 
+        // control input
+        auto const u = calc_input();
 
+        // get observation and apply noise to input
+        auto [z, ud] = observation(xTrue, xDeadReckoning, u);
 
-    //  // control input
-    //  Eigen::Vector2f u;
-    //  u<<1.0, 0.1;
-    //
-    //  // nosie control input
-    //  Eigen::Vector2f ud;
-    //
-    //  // observation z
-    //  Eigen::Vector2f z;
-    //
-    //  // dead reckoning
-    //  Eigen::Vector4f xDR;
-    //  xDR<<0.0,0.0,0.0,0.0;
-    //
-    //  // ground truth reading
-    //  Eigen::Vector4f xTrue;
-    //  xTrue<<0.0,0.0,0.0,0.0;
-    //
-    //  // Estimation
-    //  Eigen::Vector4f xEst;
-    //  xEst<<0.0,0.0,0.0,0.0;
-    //
-    //  std::vector<Eigen::Vector4f> hxDR;
-    //  std::vector<Eigen::Vector4f> hxTrue;
-    //  std::vector<Eigen::Vector4f> hxEst;
-    //  std::vector<Eigen::Vector2f> hz;
-    //
-    //  Eigen::Matrix4f PEst = Eigen::Matrix4f::Identity();
-    //
-    //  // Motional model covariance
-    //  Eigen::Matrix4f Q = Eigen::Matrix4f::Identity();
-    //  Q(0,0)=0.1 * 0.1;
-    //  Q(1,1)=0.1 * 0.1;
-    //  Q(2,2)=(1.0/180 * M_PI) * (1.0/180 * M_PI);
-    //  Q(3,3)=0.1 * 0.1;
-    //
-    //  // Observation model covariance
-    //  Eigen::Matrix2f  R = Eigen::Matrix2f::Identity();
-    //  R(0,0)=1.0;
-    //  R(1,1)=1.0;
-    //
-    //  // Motion model simulation error
-    //  Eigen::Matrix2f Qsim = Eigen::Matrix2f::Identity();
-    //  Qsim(0,0)=1.0;
-    //  Qsim(1,1)=(30.0/180 * M_PI) * (30.0/180 * M_PI);
-    //
-    //  // Observation model simulation error
-    //  Eigen::Matrix2f Rsim = Eigen::Matrix2f::Identity();
-    //  Rsim(0,0)=0.5 * 0.5;
-    //  Rsim(1,1)=0.5 * 0.5;
-    //
-    //  std::random_device rd{};
-    //  std::mt19937 gen{rd()};
-    //  std::normal_distribution<> gaussian_d{0,1};
-    //
-    //  //for visualization
-    //  cv::namedWindow("ekf", cv::WINDOW_NORMAL);
-    //  int count = 0;
-    //
-    //  while(time <= SIM_TIME){
-    //    time += DT;
-    //
-    //    ud(0) = u(0) + gaussian_d(gen) * Qsim(0,0);
-    //    ud(1) = u(1) + gaussian_d(gen) * Qsim(1,1);
-    //
-    //    xTrue = motion_model(xTrue, u);
-    //    xDR = motion_model(xDR, ud);
-    //
-    //    z(0) = xTrue(0) + gaussian_d(gen) * Rsim(0,0);
-    //    z(1) = xTrue(1) + gaussian_d(gen) * Rsim(1,1);
-    //
-    //    ekf_estimation(xEst, PEst, z, ud, Q, R);
-    //
-    //    hxDR.push_back(xDR);
-    //    hxTrue.push_back(xTrue);
-    //    hxEst.push_back(xEst);
-    //    hz.push_back(z);
-    //
-    //    //visualization
-    //    cv::Mat bg(1280,720, CV_8UC3, cv::Scalar(255,255,255));
-    //    for(unsigned int j=0; j<hxDR.size(); j++){
-    //
-    //      // green groundtruth
-    //      cv::circle(bg, cv_offset(hxTrue[j].head(2), bg.cols, bg.rows),
-    //                 7, cv::Scalar(0,255,0), -1);
-    //
-    //      // blue estimation
-    //      cv::circle(bg, cv_offset(hxEst[j].head(2), bg.cols, bg.rows),
-    //                 10, cv::Scalar(255,0,0), 5);
-    //
-    //      // black dead reckoning
-    //      cv::circle(bg, cv_offset(hxDR[j].head(2), bg.cols, bg.rows),
-    //                 7, cv::Scalar(0, 0, 0), -1);
-    //    }
-    //
-    //    // red observation
-    //    for(unsigned int i=0; i<hz.size(); i++){
-    //      cv::circle(bg, cv_offset(hz[i], bg.cols, bg.rows),
-    //               7, cv::Scalar(0, 0, 255), -1);
-    //    }
-    //
-    //    ellipse_drawing(bg, PEst.block(0,0,2,2), xEst.head(2));
-    //
-    //    cv::imshow("ekf", bg);
-    //    cv::waitKey(5);
-    //
-    //    // std::string int_count = std::to_string(count);
-    //    // cv::imwrite("./pngs/"+std::string(5-int_count.length(), '0').append(int_count)+".png", bg);
-    //
-    //    count++;
-    //  }
+        // apply ekf to update x and p estimates
+        ekf_estimation(xEst, PEst, z, ud);
+
+        std::cout << "updated position " << xEst << std::endl;
+    }
 }
