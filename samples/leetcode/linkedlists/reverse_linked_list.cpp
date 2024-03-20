bb24d3d7bc6365dce82ceaa7158a86ad8b50d1d7
Sameh Mohamed <sameh.mohamed@motional.com>
2024-03-19 22:25:03 -0400

    adding stacks

---
+++ samples/leetcode/linkedlists/reverse_linked_list.cpp
@@ -0,0 +1,35 @@
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode() : val(0), next(nullptr) {}
+*     ListNode(int x) : val(x), next(nullptr) {}
+*     ListNode(int x, ListNode *next) : val(x), next(next) {}
+* };
+*/
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+   ListNode* reverseList(ListNode* head) {
+        ListNode* curr;
+        ListNode* next = head->next;
+        while (next != nullptr) {
+            next
+        }
+   }
+};
+
+int main() {
+
+    return 0;
+}
