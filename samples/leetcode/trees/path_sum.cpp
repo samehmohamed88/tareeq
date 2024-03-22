e295405cabc228eb7c1084e487a07380e36d71e3
Sameh Mohamed <sameh.mohamed@motional.com>
2024-03-21 20:32:57 -0400

    path sum

--- samples/leetcode/trees/path_sum.cpp
+++ samples/leetcode/trees/path_sum.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 
 struct TreeNode
 {
@@ -21,14 +22,37 @@ struct TreeNode
     {}
 };
 
-class Solution
-{
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
 public:
-    TreeNode* hasPathSum(TreeNode* root, int val) {
-        
-    }
-};
+    bool hasPathSum(TreeNode* root, int targetSum) {
+        if (root == nullptr) {
+            return false;
+        }
 
+        if (root->left == nullptr && root->right == nullptr) {
+            return root->val == targetSum;
+        }
+        if (hasPathSum(root->left, targetSum - root->val)) {
+            return true;
+        }
+        if (hasPathSum(root->right, targetSum - root->val)) {
+            return true;
+        }
+        return false;
+    }
+
+};
 int main() {
 
     auto one = TreeNode(1);
@@ -46,7 +70,8 @@ int main() {
     TreeNode root = TreeNode(5, &four_again, &eight);
 
     auto sol = Solution();
-    auto node = sol.searchBST(&root, 2);
+    bool node = sol.hasPathSum(&root, 22);
 
+    std::cout << std::boolalpha << node << std::endl;
     return 0;
 }
