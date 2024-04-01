519703c55c9872401cd44a1a39dacdb564a4b7bb
Sameh Mohamed <sameh.mohamed@motional.com>
2024-03-31 21:08:41 -0400

    ready to test on jetson after adding unit tests

--- platform/devices/BoostNetworkDevice.hpp
+++ platform/devices/BoostNetworkDevice.hpp
@@ -2,7 +2,11 @@
 
 #include "platform/devices/ISerialPort.hpp"
 
+#include <boost/beast/core.hpp>
+#include <boost/beast/http.hpp>
+#include <boost/beast/version.hpp>
 #include <boost/asio.hpp>
+#include <boost/beast.hpp>
 
 #include <array>
 #include <memory>
@@ -10,8 +14,14 @@
 #include <string>
 #include <thread>
 #include <utility>
+#include <iostream>
 
 namespace platform::devices {
+
+namespace beast = boost::beast; // from <boost/beast.hpp>
+namespace http = beast::http; // from <boost/beast/http.hpp>
+namespace net = boost::asio; // from <boost/asio.hpp>
+using tcp = net::ip::tcp; // from <boost/asio/ip/tcp.hpp>
 
 template<typename AsioOperations, typename ILogger>
 class BoostNetworkDevice : public ISerialPort
@@ -88,19 +98,63 @@ void BoostNetworkDevice<AsioOperations, ILogger>::initialize()
 }
 
 template<typename AsioOperations, typename ILogger>
-void BoostNetworkDevice<AsioOperations, ILogger>::write(const std::string& request)
+void BoostNetworkDevice<AsioOperations, ILogger>::write(const std::string& command)
 {
-    logger_->logInfo("BoostNetworkDevice::write data " + request);
+    logger_->logInfo("BoostNetworkDevice::write data " + command);
     std::lock_guard<std::mutex> lock(write_mutex_);
+//    try {
+//        logger_->logInfo("Sending HTTP request: " + request);
+////        boost::asio::write(socket_, boost::asio::buffer(request));
+//        asioOperations_->template write<boost::asio::ip::tcp::socket>(socket_, boost::asio::buffer(request));
+//    } catch (const boost::system::system_error& e) {
+//        logger_->logError("BoostNetworkDevice : Error while writing: " + request);
+//        logger_->logError("BoostNetworkDevice : Error while writing: " + std::string(e.what()));
+//        throw e;
+//    }
+
     try {
-        logger_->logInfo("Sending HTTP request: " + request);
-//        boost::asio::write(socket_, boost::asio::buffer(request));
-        asioOperations_->template write<boost::asio::ip::tcp::socket>(socket_, boost::asio::buffer(request));
-    } catch (const boost::system::system_error& e) {
-        logger_->logError("BoostNetworkDevice : Error while writing: " + request);
-        logger_->logError("BoostNetworkDevice : Error while writing: " + std::string(e.what()));
-        throw e;
+        std::string target = "/js?json=" + command;
+        std::string host = server_ + ":" + port_;
+
+        // Set up an HTTP GET request message
+        http::request<http::string_body> req{http::verb::get, target, 11};
+        req.set(http::field::host, host);
+        req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+        // Look up the domain name
+        auto const results = resolver_.resolve(server_, port_);
+
+        // Make the connection on the IP address we get from a lookup
+        net::connect(socket_, results.begin(), results.end());
+
+        // Send the HTTP request to the remote host
+        http::write(socket_, req);
+
+        // This buffer is used for reading and must be persisted
+        beast::flat_buffer buffer;
+
+        // Declare a container to hold the response
+        http::response<http::dynamic_body> res;
+
+        // Receive the HTTP response
+        http::read(socket_, buffer, res);
+
+        // Write the message to standard out
+        std::cout << res << std::endl;
+
+        // Gracefully close the socket
+        beast::error_code ec;
+        socket_.shutdown(tcp::socket::shutdown_both, ec);
+
+        // not_connected happens sometimes so don't bother reporting it.
+        if (ec && ec != beast::errc::not_connected)
+            throw beast::system_error{ec};
+
+        // If we get here then the connection is closed gracefully
+    } catch (std::exception const& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
     }
+
 }
 
 template<typename AsioOperations, typename ILogger>
