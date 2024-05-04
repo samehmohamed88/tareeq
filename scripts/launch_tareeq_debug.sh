71169f7701646c85533f0476146cd230ae69cb83
Sameh Mohamed <sameh.mohamed@motional.com>
2024-05-04 10:19:19 -0400

    WIP

--- scripts/launch_tareeq_debug.sh
+++ scripts/launch_tareeq_debug.sh
@@ -1,3 +1,6 @@
+export ROS_LOG_DIR=/workspace/logs
+
 ros2 launch foxglove_bridge foxglove_bridge_launch.xml &
 
-ros2 launch tareeq tareeqav_launch.py --log-dir /workspace/logs
+ros2 launch tareeq tareeqav_launch.py
+
